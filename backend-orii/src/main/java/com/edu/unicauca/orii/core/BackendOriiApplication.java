59b71ab99c752cf5e54f07946d8057627d47014c
carlosHernandez3101 <cehernandez@unicauca.edu.co>
2024-09-12 12:56:20 -0500

    Core del sistema

---
+++ backend-orii/src/main/java/com/edu/unicauca/orii/core/BackendOriiApplication.java
@@ -0,0 +1,13 @@
+package com.edu.unicauca.orii.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOriiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOriiApplication.class, args);
+	}
+
+}
