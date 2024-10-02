59934a6c1cf1147da0e7003296e7a8d50e4626aa
Alejandro Fernández <108556384+AlejFernandezV@users.noreply.github.com>
2024-10-01 22:10:08 -0500

    Adding AgreementQueryController test

--- backend-orii/src/test/java/com/edu/unicauca/orii/core/mobility/infrastructure/adapters/input/rest/controller/AgreementCommandControllerCreateIntegrationTest.java
+++ backend-orii/src/test/java/com/edu/unicauca/orii/core/mobility/infrastructure/adapters/input/rest/controller/AgreementCommandControllerCreateIntegrationTest.java
@@ -1,8 +1,7 @@
 package com.edu.unicauca.orii.core.mobility.infrastructure.adapters.input.rest.controller;
 
-import com.edu.unicauca.orii.core.mobility.domain.enums.ScopeEnum;
-import com.edu.unicauca.orii.core.mobility.infrastructure.adapters.input.rest.data.AgreementData;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.text.SimpleDateFormat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,9 +9,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import java.text.SimpleDateFormat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.edu.unicauca.orii.core.mobility.domain.enums.ScopeEnum;
+import com.edu.unicauca.orii.core.mobility.infrastructure.adapters.input.rest.data.AgreementData;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
