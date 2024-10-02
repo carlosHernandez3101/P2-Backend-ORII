a87e001dcdc1c211ab1ace044977875362f3f02d
Brayan Majin Gaviria <brayannmajin@gmail.com>
2024-10-01 20:32:26 -0500

    add controller documentation

--- backend-orii/src/main/java/com/edu/unicauca/orii/core/mobility/infrastructure/adapters/input/rest/data/AgreementData.java
+++ backend-orii/src/main/java/com/edu/unicauca/orii/core/mobility/infrastructure/adapters/input/rest/data/AgreementData.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 import com.edu.unicauca.orii.core.mobility.domain.enums.ScopeEnum;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
